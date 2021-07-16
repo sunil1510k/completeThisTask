107aa53c3bb46086c3dc5ab4dfb99a331355f860
nitishtest <87492819+nitishtest@users.noreply.github.com>
2021-07-16 12:41:09 +0530

    Add files via upload

---
+++ masterFile.py
@@ -0,0 +1,32 @@
+
+print("""
+Hi
+Its Easy to Learn Git.
+You can Run this file on your system.
+..................................................
+""")
+
+print('''
+Also, copy below code on your Python IDE and RUN.
+Then submit the file back to the MASTER Link.
+..................................................
+''')
+
+###########################
+###########################
+###########################
+
+######### COPY ENTIRE SECTION BELOW #######
+
+print("TAERG gniod era uoY"[ : : -1])
+print("This is a simple maths power syntax")
+print(3 ** 3)
+
+
+
+
+import time
+time.sleep(5)
+
+
+
